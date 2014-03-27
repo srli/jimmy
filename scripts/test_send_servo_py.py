0ce2812ffff9eb26949774bf6656d41578c43c80
Sophia Li <sophia.li@students.olin.edu>
2014-03-27 17:14:42 -0400

    got gesture files working!

--- scripts/test_send_servo_py.py
+++ scripts/test_send_servo_py.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('jimmy')
 import rospy
-#from std_msgs.msg import String
+from std_msgs.msg import String
 from jimmy.msg import *
 
-def send_servo():
+def send_servo(data):
     pub = rospy.Publisher('jimmy_move_servo', jimmy_servo)
-    rospy.init_node('test_send_servo_cmd_py')
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        raw_input_command = raw_input("Please input desired servo and position: ")
-        input_command = raw_input_command.split()
+        input_command = str(data).split()
         print input_command
         if input_command[0] == "end":
             break
-        i = 0
+        i = 1
         msg = jimmy_servo()
         while i < len(input_command):
-            msg.positions.append(int(input_command[i+1]))
+            msg.positions.append(float(input_command[i+1]))
             if input_command[i].isalpha():
                 print "adding this to servo names"
                 msg.servo_names.append(input_command[i])
@@ -25,20 +23,20 @@ def send_servo():
                 print "adding this to servo numbers"
                 msg.servo_numbers.append(int(input_command[i]))
             i += 2 
-#        rospy.loginfo(msg)
+        rospy.loginfo(msg)
         pub.publish(msg)
+        print "message published!"
         r.sleep()
+        
+def main():
+    rospy.init_node('test_send_servo_cmd_py')
+    rospy.Subscriber("jimmy_move_gesture", String, send_servo)
+    print "Waiting for commands!"
+
+    rospy.spin()
+        
         
 if __name__ == '__main__':
     try:
-#        pub = rospy.Publisher('jimmy_move_servo',jimmy_servo)
-#        rospy.init_node('test_send_servo_cmd_py')
-#        r = rospy.Rate(1) # 10hz
-#        while not rospy.is_shutdown():
-#            msg = jimmy_servo()
-#            msg.servo_names.append("LeftElbow")
-#            msg.positions.append(0.5)
-#            pub.publish(msg)
-#            r.sleep()
-        send_servo()
+        main()
     except rospy.ROSInterruptException: pass

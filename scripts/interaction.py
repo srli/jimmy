1983dc1e1766917259dd4b68fe473720b1bf3486
srli <sophia.li@students.olin.edu>
2015-02-11 23:22:19 -0500

    adding test kinect files

--- scripts/interaction.py
+++ scripts/interaction.py
@@ -8,28 +8,28 @@ import random
 
 def interaction_callback(data):
     global message
-    # wave = data.wave
-    # hello = data.hello
-    # goodbye = data.goodbye
-    # #our message fields are all booleans so we can do easy if statements
-    # if wave:
-    #     print "waved!"
-    #     message = 12#random.randint(8, 11) #sets messages as desired as reponses to detected gestures
-    # if hello:
-    #     print "hello!"
-    #     message = 13
-    # if goodbye:
-    #     print "goodbye"
-    #     message = 14
-    # if not goodbye and not hello and not wave:
-    #     print "none work"
-    #     message = 0
+    wave = data.wave
+    hello = data.hello
+    goodbye = data.goodbye
+    #our message fields are all booleans so we can do easy if statements
+    if wave:
+        print "waved!"
+        message = 12#random.randint(8, 11) #sets messages as desired as reponses to detected gestures
+    if hello:
+        print "hello!"
+        message = 13
+    if goodbye:
+        print "goodbye"
+        message = 14
+    if not goodbye and not hello and not wave:
+        print "none work"
+        message = 0
     print message
 
 def publisher():
     global message
     rospy.init_node("interaction_node", anonymous = True)
-    rospy.Subscriber("/detected_gestures", gestures, interaction_callback) #subscribing to detected gestures from detectfinger
+    rospy.Subscriber("/detected_gestures", jimmy_gesture, interaction_callback) #subscribing to detected gestures from detectfinger
     pub = rospy.Publisher('/jimmy_idle', jimmy_gesture)
     r = rospy.Rate(20)
     while not rospy.is_shutdown():

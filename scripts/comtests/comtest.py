81946dd7cb6ce3712030c1ab8d5db36236c07d43
srli <sophia.li@students.olin.edu>
2015-03-01 17:14:34 -0500

    added dynamixel manager

--- scripts/comtests/comtest.py
+++ scripts/comtests/comtest.py
@@ -12,28 +12,28 @@ s.open()
 # set register values
 def setReg(ID,reg,values,index):
     length = 3 + len(values)
-    checksum = 255-((index+length+AX_WRITE_DATA+reg+sum(values))%256)          
+    checksum = 255-((index+length+AX_WRITE_DATA+reg+sum(values))%256)
     s.write(chr(0xFF)+chr(0xFF)+chr(ID)+chr(length)+chr(AX_WRITE_DATA)+chr(reg))
     for val in values:
         s.write(chr(val))
     s.write(chr(checksum))
 
 def getReg(index, regstart, rlength):
-    s.flushInput()   
+    s.flushInput()
     checksum = 255 - ((6 + index + regstart + rlength)%256)
     s.write(chr(0xFF)+chr(0xFF)+chr(index)+chr(0x04)+chr(AX_READ_DATA)+chr(regstart)+chr(rlength)+chr(checksum))
     vals = list()
     s.read()   # 0xff
-    print 'read'
-    s.read()   # 0xff
-    print 'read'
-    print s.read()   # ID
-    print 'read'
+    # print 'read'
+    # s.read()   # 0xff
+    # print 'read'
+    # print s.read()   # ID
+    # print 'read'
     length = ord(s.read()) - 1
-    print length
-    s.read()   # toss error 
-    
-    it = 0  
+    # print length
+    s.read()   # toss error
+
+    it = 0
     while length > 0:
         print 'it', it
         it += 1
@@ -44,5 +44,9 @@ def getReg(index, regstart, rlength):
     return vals
 
 index = 1
-setReg(1,30,((512%256),(512>>8)),index)  # this will move servo 1 to centered position (512)
-print getReg(index,43,1)               # get the temperature
+#setReg(1,30,((512%256),(512>>8)),index)  # this will move servo 1 to centered position (512)
+for i in range(1,25):
+    print "index ", i
+    print getReg(i,43,1)               # get the temperature
+
+
